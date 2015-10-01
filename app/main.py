197163185a664315cb4ed122e13a48eeb8be797a
Jacksonbaker323 <jackson@jacksonbaker.net>
2015-09-30 22:08:08 -0700

    Created some functions to load data and start parsing

--- app/main.py
+++ app/main.py
@@ -1,11 +1,29 @@
 from geopandas.geodataframe import GeoDataFrame
 from shapely.geometry import Point
+import csv
 
-states = GeoDataFrame.from_file('shapefiles/s_16de14.shp')
-states.set_index('NAME', inplace=True)
+def LoadStates():
+    #load the state shapefiles
+    states = GeoDataFrame.from_file('shapefiles/s_16de14.shp')
+    states.set_index('NAME', inplace=True)
+    return states
 
-point = Point([47.53, -120.62])
 
-state = states.contains(point).first_valid_index()
+def StateFromPoint(latitude, longitude, states):
+    #create a point
+    point = Point([longitude, latitude])
+    #create a pandas series that lists the states and if the point exists in it or nto
+    state = states.contains(point)
+    #return the name of the state that has the point in it
+    return state[state == True].first_valid_index()
 
-print(state)
+def LoadCSV(filename):
+    #load the csv into a dataframe
+    csv = pandas.read_csv(filename)
+    #add a state column
+    csv['State'] = pandas.Series()
+
+if __name__ == '__main__':
+    states = LoadStates()
+    state_name = StateFromPoint(42, -120, states)
+    print(state_name)
